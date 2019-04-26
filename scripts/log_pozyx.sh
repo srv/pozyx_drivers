5db047752f2419a5eef8bb5edaf700fbe67e157b
Francisco Bonnin-Pascual <xisco.bonnin@uib.es>
2019-04-26 09:45:30 +0200

    Files used in Munkebo March 2019

--- scripts/log_pozyx.sh
+++ scripts/log_pozyx.sh
@@ -1,4 +1,3 @@
 
-rostopic echo -p /pozyx_node_A/pose/pose > expUWB/positionA$1.txt &
-rostopic echo -p /pozyx_node_B/pose/pose > expUWB/positionB$1.txt &
+rostopic echo -p /pozyx_node/pose/pose > expUWB/position$1.txt &
 rosbag record -a -O expUWB/$1
--- scripts/plot_uwb.m
+++ scripts/plot_uwb.m
@@ -2,7 +2,7 @@
 function plot_uwb(logFile)
 
 data = load(['/home/xisco/expUWB/position',logFile, '.txt']);
-data2 = load(['/home/xisco/expUWB/filt_ICP_',logFile, '.txt']);
+%data2 = load(['/home/xisco/expUWB/filt_ICP_',logFile, '.txt']);
 
 figure(1)
 hold on;
@@ -10,9 +10,9 @@ plot3(data(:,2),data(:,3),data(:,4), 'LineWidth', 2);
 plot3(data(1,2),data(1,3),data(1,4), 'xg', 'LineWidth', 2);
 plot3(data(end,2),data(end,3),data(end,4), 'or', 'LineWidth', 2);
 
-plot3(data2(:,2),data2(:,3),data2(:,4), 'm', 'LineWidth', 2);
-plot3(data2(1,2),data2(1,3),data2(1,4), 'xg', 'LineWidth', 2);
-plot3(data2(end,2),data2(end,3),data2(end,4), 'or', 'LineWidth', 2);
+%plot3(data2(:,2),data2(:,3),data2(:,4), 'm', 'LineWidth', 2);
+%plot3(data2(1,2),data2(1,3),data2(1,4), 'xg', 'LineWidth', 2);
+%plot3(data2(end,2),data2(end,3),data2(end,4), 'or', 'LineWidth', 2);
 
 grid on;
 axis equal;
