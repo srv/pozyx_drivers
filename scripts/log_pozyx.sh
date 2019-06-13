aee4678ec26f0007201e0ec1252f1862d464a31e
Mussol MAV <mussol@uib.es>
2019-06-13 12:54:25 +0200

    Updating logging method

--- scripts/log_pozyx.sh
+++ scripts/log_pozyx.sh
@@ -1,3 +1,9 @@
+#rostopic echo -p /pozyx_node/pose/pose > expUWB/position$1.txt &
+#rosbag record -a -O expUWB/$1
 
-rostopic echo -p /pozyx_node/pose/pose > expUWB/position$1.txt &
-rosbag record -a -O expUWB/$1
+#rosbag record -e "/pozyx_node/(.*)" -O expUWB/$1
+
+cd /home/srv/expUWB
+rosbag record -e "/pozyx_node/(.*)" -o $1
+cd -
+
